36abdf6dfc82fac43da6da9d3c9ac51b56ca059e
John Casper I. Laya <111546399+JohnCasper02@users.noreply.github.com>
2022-09-06 10:09:24 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    String surname;
+    String firstName;
+    char middleInitial;
+    String dateOfBirth;
+    int studentNumber;
+    String studentEmailAddress;
+    boolean iAmAwesome;
+
+    public void Laya (){
+        System.out.println("My Student Number is" + studentNumber);
+        System.out.println("My student EmailAddres is" + studentEmailAddress);
+        System.out.println(" I am awesome " + iAmAwesome);
+        
+        
+    } 
+       
+}    
