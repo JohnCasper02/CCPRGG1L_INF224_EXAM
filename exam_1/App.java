2c6a4ba3647be7b73c30ab88405a0558565b4403
John Casper I. Laya <111546399+JohnCasper02@users.noreply.github.com>
2022-09-06 09:57:56 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,22 @@
+public class App { 
+    public static void main(String[]args) {
+
+     // Character Class
+
+     Student Laya = new Student();
+
+     Laya.surname = "Laya";
+     Laya.firstName = "John Casper";
+     Laya.middleInitial = 'I';
+     Laya.dateOfBirth = "09/02/2001";
+     Laya.studentNumber = 10443;
+     Laya.studentEmailAddress = "layaji@students.national-u.edu.ph";
+     Laya.iAmAwesome = true;
+     Laya.Laya();
+
+   
+
+    
+     
+    }
+} 
